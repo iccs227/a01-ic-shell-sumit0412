ac56dcd8481e3a9866efd38612f4ab14e753539d
sumit0412 <sumit.sachdev412@gmail.com>
2025-06-11 22:58:12 +0700

    Milestone 1 implemented: icsh

--- icsh.c
+++ icsh.c
@@ -1,17 +1,90 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name: Sumit Sachdev
+ * StudentID: 6480523
  */
 
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MAX_CMD_BUFFER 255
 
+char last_command[MAX_CMD_BUFFER];
+
+void handle_echo(char* input) {
+    char* echo_text = input + 5;
+    printf("%s", echo_text);
+}
+
+void handle_double_bang() {
+    if (strlen(last_command) > 0) {
+        printf("%s", last_command);
+        
+        char temp[MAX_CMD_BUFFER];
+        strcpy(temp, last_command);
+        temp[strcspn(temp, "\n")] = 0;
+        
+        if (strncmp(temp, "echo ", 5) == 0) {
+            handle_echo(last_command);
+        }
+        else if (strcmp(temp, "!!") != 0) {
+            strcpy(last_command, temp);
+            strcat(last_command, "\n");
+        }
+    }
+}
+
+void handle_exit(char* input) {
+    int exit_code = 0;
+    char* exit_arg = input + 5;
+    exit_arg[strcspn(exit_arg, "\n")] = 0;
+    
+    if (strlen(exit_arg) > 0) {
+        exit_code = atoi(exit_arg) & 0xFF;
+    }
+    
+    printf("bye\n");
+    exit(exit_code);
+}
+
 int main() {
     char buffer[MAX_CMD_BUFFER];
+    
+    printf("Starting IC shell\n");
+    
+    memset(last_command, 0, MAX_CMD_BUFFER);
+    
     while (1) {
         printf("icsh $ ");
-        fgets(buffer, 255, stdin);
-        printf("you said: %s\n", buffer);
+        fflush(stdout);
+        
+        if (fgets(buffer, MAX_CMD_BUFFER, stdin) == NULL) {
+            break;
+        }
+        
+        if (strcmp(buffer, "\n") == 0) {
+            continue;
+        }
+        
+        char cmd_copy[MAX_CMD_BUFFER];
+        strcpy(cmd_copy, buffer);
+        cmd_copy[strcspn(cmd_copy, "\n")] = 0;
+        
+        if (strncmp(cmd_copy, "echo ", 5) == 0) {
+            handle_echo(buffer);
+            strcpy(last_command, buffer);
+        }
+        else if (strcmp(cmd_copy, "!!") == 0) {
+            handle_double_bang();
+        }
+        else if (strncmp(cmd_copy, "exit", 4) == 0) {
+            handle_exit(buffer);
+        }
+        else {
+            printf("bad command\n");
+        }
     }
+    
+    return 0;
 }
